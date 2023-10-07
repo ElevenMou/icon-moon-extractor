fb1ce3dde529ae638397c358c0e0e772c3daff44
moussa saidi <moussa.saidi.01@gmail.com>
2023-10-07 18:13:13 +0100

    Update styling

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Header />
-            <div>{children}</div>
+            <div className="app-main">{children}</div>
         </>
     );
 };
